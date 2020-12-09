78543b1f2c514d9eb32ab1f4657ec3af66889773
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-12-08 19:25:56 -0800

    Saving progress. Endpoints remaining.

--- data/seeds/01-add_cars.js
+++ data/seeds/01-add_cars.js
@@ -8,7 +8,7 @@ exports.seed = function(knex) {
         {VIN: '5GZCZ23DX3S826679', Make: 'Pontiac', Model: 'GTO', Mileage: 12613,  Transmission_Type: 'Manual', Title_Status:'Clear'},
         {VIN: 'JTEBU5JR8E5156431', Make: 'Chrysler', Model: 'Crossfire', Mileage: 100103,  Transmission_Type: 'Automatic', Title_Status:'Salvage'},
         {VIN: '3FAFP07126R108557', Make: 'Toyota', Model: 'Yaris', Mileage: 206013,  Transmission_Type: 'Automatic', Title_Status:'Reconstructed'},
-        {VIN: '19XFB2F53EE058119', Make: 'Mazda', Model: 'MX-5', Mileage: 56130,  Transmission_Type: 'Automatic', Title_Status:''},
+        {VIN: '19XFB2F53EE058119', Make: 'Mazda', Model: 'MX-5', Mileage: 56130,  Transmission_Type: 'Automatic'},
       ]);
     });
 };
